e747bbdc230dbdbad12bd68339733384cd0b0c5f
windupratama <winduprtma@gmail.com>
2024-04-29 00:33:52 +0700

    update

--- api/index.js
+++ api/index.js
@@ -3,12 +3,10 @@ const express = require('express');
 const app = express()
 const PORT = 8000
 
-app.get('/', (req, res) => {
-  res.send('Hello World')
-})
+app.use(express.json());
 
-app.get('/about', (req, res) => {
-  res.send('About route 🎉 ')
+app.get('/', (req, res) => {
+  res.json({ message: 'Hello World' });
 })
 
 app.listen(PORT, () => {
